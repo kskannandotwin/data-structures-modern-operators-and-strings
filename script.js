c0d7f33bcb4edf06e3086527dfeba14a21b16598
kskannandotwin <kskannan.win@gmail.com>
2024-02-27 07:39:08 +0530

    27.02.2024
    tutorial initiated

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pissa', 'Pasta', 'Risotto']
+};
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+const [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+const temp = main;
+main = secondary;
+secondary = temp;
