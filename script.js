adc27e18a5a6257525b1abf226bd24912b39102a
kskannandotwin <kskannan.win@gmail.com>
2024-03-16 13:52:14 +0530

    16.03.2024-tnco
    completed

--- script.js
+++ script.js
@@ -43,32 +43,11 @@ const restaurant = {
     console.log(otherIngredients);
   }
 };
-console.log('------- OR ------');
-// use ANY data type, return ANY data type
-// short-circuiting
-console.log(3 || 'Jonas');
-console.log('' || 'Jonas');
-console.log(true || 0);
-console.log(undefined || null);
-
-console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
 restaurant.numGuests = 0;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
-
 const guests2 = restaurant.numGuests || 10;
 console.log(guests2);
 
-console.log('------- AND ------');
-console.log(0 && 'Jonas');
-console.log(7 && 'Jonas');
-
-console.log('Hello' && 23 && null && 'jonas');
-
-// practical example
-if(restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
-}
-
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
