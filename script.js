9732dcab67847659e2bbaff64eec1b48362da1ab
kskannandotwin <kskannan.win@gmail.com>
2024-03-16 14:26:32 +0530

    16.03.2024-lao
    completed

--- script.js
+++ script.js
@@ -44,10 +44,34 @@ const restaurant = {
   }
 };
 
-restaurant.numGuests = 0;
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20
+  numGuests: 0
+}
 
-// nullish: null and undefined (NOT 0 or '')
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rosi'
+}
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && 'ANONYMOUS';
+// rest2.owner = rest2.owner && 'ANONYMOUS';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
