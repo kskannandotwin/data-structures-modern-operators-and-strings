38465a37c6eb3fd6d3e73f0f127a76148c0c6279
kskannandotwin <kskannan.win@gmail.com>
2024-03-17 21:15:39 +0530

    17.03.2024-cc#1
    completed

--- script.js
+++ script.js
@@ -1,77 +1,144 @@
 'use strict';
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+// Coding Challenge #1
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+/* 
+We're building a football betting app (soccer for my American friends)!
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
-  },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  }
 };
 
-const rest1 = {
-  name: 'Capri',
-  // numGuests: 20
-  numGuests: 0
+// 1. Create one player array for each team
+// (variables 'players1' and 'players2')
+const [players1, players2] = game.players; // test passed
+console.log(players1, players2);
+
+// 2. The first player in any player array is the goalkeeper and the others
+// are field players. For Bayern Munich (team 1) create one variable ('gk')
+// with the goalkeeper's name, and one array ('fieldPlayers') with all the
+// remaining 10 field players
+// const [gkNeuer, ...fieldPlayers1] = players1; // my answer
+// console.log(gkNeuer, fieldPlayers1);
+
+// tutorial answer
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3. Create an array 'allPlayers' containing all players of both
+// teams (22 players)
+const allPlayers = [...players1, ...players2]; // test passed
+console.log(allPlayers);
+
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players.
+// So create a new array ('players1Final') containing all the original team1
+// players plus 'Thiago', 'Coutinho' and 'Perisic'
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic']; // test passed
+console.log(players1Final);
+
+// 5. Based on the game.odds object, create one variable for each odd
+// (called 'team1', 'draw' and 'team2')
+// below my answer totally wrong as per the tutorial
+/*
+const team1 = game.odds.team1;
+console.log(team1);
+
+const draw = game.odds.x;
+console.log(draw);
+
+const team2 = game.odds.team2;
+console.log(team2);
+*/
+// tutorial answer below for the above
+const {odds: {team1, x: draw, team2}} = game;
+console.log(team1, draw, team2);
+
+// 6. Write a function ('printGoals') that receives an arbitrary number
+// of player names (NOT an array) and prints each of them to the console,
+// along with the number of goals that were scored in total (number of player
+// names passed in)
+// below is gemini way
+/*
+function printGoals(...players) {
+  // Count the total players (arguments passed)
+  const numPlayers = players.length;
+  console.log(`There were ${numPlayers} goals scored in total.`);
+  
+  // Loop through each player and print their name
+  for (const player of players) {
+    console.log(player);
+  }
 }
+*/
 
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rosi'
-}
+// Example usage by gemini way
+// printGoals("Alice", "Bob", "Charlie");  // Output: There were 3 goals scored in total. Alice Bob Charlie
 
-// OR assignment operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
+// tutorial way for the above question
+const printGoals = function(...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored.`)
+};
 
-// rest1.numGuests ||= 10;
-// rest2.numGuests ||= 10;
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals('Davies', 'Muller');
+printGoals(...game.scored);
 
-// nullish assignment operator (null or undefined)
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
+// 7. The team with the lower odd is more likely to win. Print to the console
+// which team is more likely to win, WITHOUT using an if/else statement or the
+// ternary operator.
 
-// AND assignment operator
-// rest1.owner = rest1.owner && 'ANONYMOUS';
-// rest2.owner = rest2.owner && 'ANONYMOUS';
-
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
-
-console.log(rest1);
-console.log(rest2);
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
