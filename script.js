d2c8fe5d1b0a6b31f78640d3be7a05a323e3b99d
kannanATexcelforte <kannan@excelforte.com>
2024-03-18 17:08:04 +0530

    18.03.2024-oc
    completed

--- script.js
+++ script.js
@@ -49,3 +49,31 @@ const restaurant = {
     console.log(otherIngredients);
   }
 };
+
+if (restaurant.openingHours && restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// example
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}.`);
+}
+
+// methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist.');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist.');
+
+// arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// const users = [];
+console.log(users[0]?.name ?? 'User array is empty.');
+
+if(users.length > 0) console.log(users[0].name); else console.log('user array empty');
