ff12d35ef3f61588bf0d1b6d2d132f254bd0d93c
kskannandotwin <kskannan.win@gmail.com>
2024-03-18 07:05:02 +0530

    18.03.2024-latfol
    completed

--- script.js
+++ script.js
@@ -44,34 +44,12 @@ const restaurant = {
   }
 };
 
-const rest1 = {
-  name: 'Capri',
-  // numGuests: 20
-  numGuests: 0
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for(const item of menu) console.log(item);
+
+for(const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
 }
 
-const rest2 = {
-  name: 'La Piazza',
-  owner: 'Giovanni Rosi'
-}
-
-// OR assignment operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
-
-// rest1.numGuests ||= 10;
-// rest2.numGuests ||= 10;
-
-// nullish assignment operator (null or undefined)
-rest1.numGuests ??= 10;
-rest2.numGuests ??= 10;
-
-// AND assignment operator
-// rest1.owner = rest1.owner && 'ANONYMOUS';
-// rest2.owner = rest2.owner && 'ANONYMOUS';
-
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
-
-console.log(rest1);
-console.log(rest2);
+// console.log([...menu.entries()]);
