7a0e3d99f013f68ffc9433f1f733501a124aef14
kskannandotwin <kskannan.win@gmail.com>
2024-03-19 07:49:09 +0530

    19.03.2024-lookvae
    completed

--- script.js
+++ script.js
@@ -49,3 +49,28 @@ const restaurant = {
     console.log(otherIngredients);
   }
 };
+
+// property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days:`;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+// property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// [key, value]
+for (const [day, {open, close}] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
